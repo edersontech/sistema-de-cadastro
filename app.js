5c328e37d2e1231506b8c89d3b49a991870b5b53
edersontech <86799279+edersontech@users.noreply.github.com>
2024-12-13 16:45:06 -0300

    console log indicando modificacoes do apulo

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerencimaneto de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+// mais um console log teste:
+console.log("O paulo fez um commit sem voce ver na aula")
