a6b15ff3464505b9cbfa6c4005902cf62edd0ce0
edersontech <ederson_tech@hotmail.com>
2024-12-13 16:54:38 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerencimaneto de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 // mais um console log teste:
 console.log("O paulo fez um commit sem voce ver na aula")
+console.log("Enviando para o GitHub")
