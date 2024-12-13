0830b31632ad0a6c966dfca78d596d8df5968b15
edersontech <ederson_tech@hotmail.com>
2024-12-13 17:08:28 -0300

    voltando para o stado do paulo

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o sistema de gerencimaneto de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 // mais um console log teste:
 console.log("O paulo fez um commit sem voce ver na aula")
-console.log("Enviando para o GitHub")
